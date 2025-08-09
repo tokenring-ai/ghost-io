ef0d08790521821b94e2a72f9f83db4f5d9283c6
Mark Dierolf <mdierolf@cloudquote.io>
2025-08-08 22:02:00 -0700

    Converted package to typescript

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export * as chatCommands from "./chatCommands.js";
+export { default as GhostIOService } from "./GhostIOService.js";
+export * as tools from "./tools.js";
+
+export const name: string;
+export const description: string;
+export const version: string;
